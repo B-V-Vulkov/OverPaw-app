3acdc4fa71f17dee9843fdb2e146d94509ab28ea
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2020-03-23 21:27:37 +0200

    add ContentModule, LoginModule, NotFoundModule and ServerErrorModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentComponent } from './pages/content/content.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', loadChildren: () => import('./pages/content/content.module').then(m => m.ContentModule) },
+    { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+    { path: '500', loadChildren: () => import('./pages/server-error/server-error.module').then(m => m.ServerErrorModule) },
+    { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
