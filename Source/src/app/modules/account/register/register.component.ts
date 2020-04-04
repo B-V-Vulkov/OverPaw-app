2f52880ef406f68945de7a8b33937a80b87a3e7b
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2020-04-04 14:01:19 +0300

    add validation

--- Source/src/app/modules/account/register/register.component.ts
+++ Source/src/app/modules/account/register/register.component.ts
@@ -1,45 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AccountService } from 'src/app/core/services/account.service';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html'
+    selector: 'app-register',
+    templateUrl: './register.component.html'
 })
 export class RegisterComponent implements OnInit {
 
-  public registerForm: FormGroup;
+    public registerForm: FormGroup;
 
-  public isInvalidLoginData: boolean = false;
+    public isInvalidLoginData: boolean = false;
 
-  constructor(
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private accountService: AccountService) {
+    constructor(
+        private router: Router,
+        private formBuilder: FormBuilder,
+        private accountService: AccountService) {
 
-    this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      isSelectedRememberMe: ['']
-    });
-  }
+        this.registerForm = this.formBuilder.group({
+            firstName: ['', Validators.required],
+            familyName: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]],
+            isSelectedRememberMe: ['']
+        });
+    }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+    }
 
+    addValidationClass(control: AbstractControl): string {
+        if (!control.touched) {
+            return '';
+        }
+        if (control.valid) {
+            return 'is-valid';
+        }
+        return 'is-invalid';
+    }
 
-  }
+    isInvalidAndTouched(control: AbstractControl): boolean {
+        return (control.invalid && control.touched);
+    }
 
-  onSubmitRegisterForm() {
-  }
+    test(abstractControl: AbstractControl): boolean {
+        return (abstractControl.valid && abstractControl.touched);
+    }
 
-  registerFormControl() {
-    return this.registerForm.controls;
-  }
+    onSubmitRegisterForm() {
+    }
 
-  navigateToLogin() {
-    this.router.navigate(['./account/login']);
-  }
+    registerFormControl() {
+        return this.registerForm.controls;
+    }
+
+    navigateToLogin() {
+        this.router.navigate(['./account/login']);
+    }
 
 
 
